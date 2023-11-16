72cac535afb5b2a6780c6e3d2c7d37f6797ef880
piotex <pkubon2@gmail.com>
2023-11-16 20:16:46 +0100

    text to speech poc

---
+++ text_to_speach.py
@@ -0,0 +1,12 @@
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("now we're talkin'")
+# engine.runAndWait()
+
+
+# import pytdm
+# # pytdm.mów("dzień dobry, dobranoc")
+# pytdm.zapisz("dzień dobry świecie!", "example.mp3")
+
+
+
